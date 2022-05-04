95310514d1803cbc621b0c0806839d615f73326b
Crezzas <104891318+Crezzas@users.noreply.github.com>
2022-05-04 11:44:14 +0100

    App 1

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello from Version 2
+          Hello from Version 3
         </h1>
-        <p>Its Mark</p>
+        <p>Its Mark Again</p>
       </header>
     </div>
   );
